db9694699f67dee4518b77abf379565846335c51
Guusvanmeerveld <mail@guusvanmeerveld.dev>
2022-03-19 01:59:41 +0100

    Basic setup for daemon package

---
+++ packages/daemon/rollup.config.ts
@@ -0,0 +1,23 @@
+import run from '@rollup/plugin-run';
+
+import json from '@rollup/plugin-json';
+
+import typescript from 'rollup-plugin-typescript2';
+import tsConfigPaths from 'rollup-plugin-ts-paths';
+
+import { terser } from 'rollup-plugin-terser';
+
+const dev = process.env.ROLLUP_WATCH === 'true';
+
+/**
+ * @type {import('rollup').RollupOptions}
+ */
+export default {
+	input: 'src/index.ts',
+	output: {
+		dir: 'dist',
+		format: 'cjs',
+	},
+	external: [],
+	plugins: [dev && run(), json(), typescript(), tsConfigPaths(), terser()],
+};
